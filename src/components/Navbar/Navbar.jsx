b8b729e74153e3470972420548963110af1587ff
menna5252 <menna.route@gmail.com>
2025-07-17 21:03:09 +0300

    first commit2555

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink className="nav-link fw-semibold fs-5" aria-current="page" to="">Home</NavLink>
+          <NavLink className="nav-link fw-semibold fs-5" aria-current="page" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
           <NavLink className="nav-link fw-semibold fs-5" to="/about">About</NavLink>
